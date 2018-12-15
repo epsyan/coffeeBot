2b46dac88af7c6304f6d66faa4a5f38ddb8a14f3
Your Name <you@example.com>
2018-12-15 11:53:30 +0100

    update messages

--- msg.js
+++ msg.js
@@ -4,7 +4,9 @@ module.exports = {
 		'*roll* - choose someone who will make coffee. \n' +
 		'*ok* - agree with lottery and save stats. Chosen one can\'t save stats. \n' +
 		'*stats* - show stats in format like: _name_ | _coffeeMakeCounter_ | _lastMakeAt_ . \n' +
-		'*rollModel* - show results for a 1000 times random choise.'
+		'*rollModel* - show results for a 1000 times random choise. \n' + 
+		'*news* - print latest news list. \n' +
+		'*vacation %personName%* - set vacation status for person. You should use just name, without @ sign.'
 	),
 	TIME_FOR_COFFEE: 'Time for coffee! <@%s> you are the chosen one. \n (_%s was excluded from lottery cos he made coffee last time_)',
 	CHOSEN_ONE_CANT_SAVE: 'Chosen one can\'t save stats. Let someone else type "ok" command to me.',
